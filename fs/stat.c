cb7badeac6d4f838d35251be7afcab5f5dec12ad
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-03-20 08:27:25 +0800

    fs: susfs: Refine SUS_KSTAT !!! This commit requires the updated ksu_susfs binary !!!
    - Use RCU to read and modify SUS_KSTAT_HLIST.
    
    - Reorder struct st_susfs_sus_kstat and add new flags member to determine for which stat attribute to spoof.
    
    - Rename and refine "void susfs_sus_ino_for_show_map_vma(unsigned long ino, dev_t *out_dev, unsigned long *out_ino);" => "void susfs_show_map_vma_spoofer(struct inode *inode, dev_t *out_dev, unsigned long *out_ino);".
    
    - Rename and refine "void susfs_sus_ino_for_generic_fillattr(unsigned long ino, struct kstat *stat);" => "void susfs_generic_fillattr_spoofer(struct inode *inode, struct kstat *stat);".
    
    - Add one more hook in vfs_getattr_nosec() as it is needed to spoof for some FS like FUSE.
    
    Co-Authored by: simonpunk <simonpunk2016@gmail.com>
    
    (cherry picked from commit b53211dd515d462c3e992bcedb618c28be119d8f)

--- fs/stat.c
+++ fs/stat.c
@@ -32,23 +32,11 @@
  * operation is supplied.
  */
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-extern void susfs_sus_ino_for_generic_fillattr(unsigned long ino, struct kstat *stat);
+extern void susfs_generic_fillattr_spoofer(struct inode *inode, struct kstat *stat);
 #endif
 
 void generic_fillattr(struct inode *inode, struct kstat *stat)
 {
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-	if (unlikely(test_bit(AS_FLAGS_SUS_KSTAT, &inode->i_state)) &&
-		likely(susfs_is_current_proc_umounted_app()))
-	{
-		susfs_sus_ino_for_generic_fillattr(inode->i_ino, stat);
-		stat->mode = inode->i_mode;
-		stat->rdev = inode->i_rdev;
-		stat->uid = inode->i_uid;
-		stat->gid = inode->i_gid;
-		return;
-	}
-#endif
 	stat->dev = inode->i_sb->s_dev;
 	stat->ino = inode->i_ino;
 	stat->mode = inode->i_mode;
@@ -62,6 +50,14 @@ void generic_fillattr(struct inode *inode, struct kstat *stat)
 	stat->ctime = inode->i_ctime;
 	stat->blksize = i_blocksize(inode);
 	stat->blocks = inode->i_blocks;
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+	susfs_generic_fillattr_spoofer(inode, stat);
+#endif
+
+	if (IS_NOATIME(inode))
+		stat->result_mask &= ~STATX_ATIME;
+	if (IS_AUTOMOUNT(inode))
+		stat->attributes |= STATX_ATTR_AUTOMOUNT;
 }
 
 EXPORT_SYMBOL(generic_fillattr);
@@ -83,8 +79,18 @@ int vfs_getattr_nosec(struct path *path, struct kstat *stat)
 	struct inode *inode = d_backing_inode(path->dentry);
 
 	if (inode->i_op->getattr)
-		return inode->i_op->getattr(path->mnt, path->dentry, stat);
-
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+	{
+		int err = inode->i_op->getattr(path, stat, request_mask,
+					    query_flags);
+		if (!err)
+			susfs_generic_fillattr_spoofer(inode, stat);
+		return err;
+	}
+#else
+		return inode->i_op->getattr(path, stat, request_mask,
+					    query_flags);
+#endif
 	generic_fillattr(inode, stat);
 	return 0;
 }
